998d2b326d546dc20247dca72d518e4a4c40abe7
Damien Cassou <damien@cassou.me>
2021-01-28 22:07:51 +0100

    Temporarily disable unit tests to focus on doctests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,23 +1,22 @@
-import qualified Test.Tasty as Tasty
-import           Test.Tasty.Hspec (shouldBe, describe, it, testSpec)
-import           Data.List (sort)
+-- import qualified Test.Tasty as Tasty
+-- import           Test.Tasty.Hspec (shouldBe, describe, it, testSpec)
+-- import           Data.List (sort)
 import           Test.DocTest as DocTest (doctest)
 import           Control.Exception (catch, throwIO)
 import           System.Exit ( ExitCode(ExitSuccess) )
 
-import Lib ( faces, Dice(One, Six, Five, Four, Three, Two) )
-
 main = do
-  tree <- testSpec "Tests" spec_dice
-  Tasty.defaultMain tree
-  `catch` (\e -> do
-              if e == ExitSuccess
-                then doctest ["-isrc", "src/Lib.hs"]
-                else throwIO e)
+  -- tree <- testSpec "Tests" spec_dice
+  -- Tasty.defaultMain tree
+  -- `catch` (\e -> do
+  --             if e == ExitSuccess
+  --               then
+                doctest ["-isrc", "src/Lib.hs"]
+                -- else throwIO e)
 
-spec_dice = do
-  describe "Face" $ do
-    it "specifies an order between elements" $
-      sort [Six, Five, Four, Three, Two, One] `shouldBe` faces
-    it "can be printed" $
-      map show faces `shouldBe` ["One", "Two", "Three", "Four", "Five", "Six"]
+-- spec_dice = do
+--   describe "Face" $ do
+--     it "specifies an order between elements" $
+--       sort [Six, Five, Four, Three, Two, One] `shouldBe` faces
+--     it "can be printed" $
+--       map show faces `shouldBe` ["One", "Two", "Three", "Four", "Five", "Six"]

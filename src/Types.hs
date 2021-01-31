56d45e802605ea5aa22b1a728f1172178fcc866b
Damien Cassou <damien@cassou.me>
2021-01-31 18:20:51 +0100

    Add ScoreCard.hs to manipulate a score card

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,18 @@
 module Types where
 
+import Data.Map
+
 type Throw = [Int]
 type Scoring = Throw -> Int
+
+data UpperFigure = Aces | Twos | Threes | Fours | Fives | Sixes
+  deriving (Eq, Show, Ord)
+
+data LowerFigure = ThreeOfAKind | FourOfAKind | SmallStraight | LargeStraight | Yahtzee | Chance
+  deriving (Eq, Show, Ord)
+
+data Figure = UFigure UpperFigure | LFigure LowerFigure
+  deriving (Eq, Show, Ord)
+
+type Box = Maybe Int
+type ScoreCard = Map Figure Box

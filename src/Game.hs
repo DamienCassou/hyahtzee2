0117b2717ff9cd9d00cad069be08faf0ce213918
Damien Cassou <damien@cassou.me>
2021-02-07 14:20:17 +0100

    Add creation function to Game.hs

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,12 @@
 module Game where
 
-import Round (Round)
-import ScoreCard (ScoreCard)
+
+import Round (Round, newRound)
+import ScoreCard (ScoreCard, newScoreCard)
+import System.Random (StdGen)
 
 data Game = Game { round :: Round, scoreCard :: ScoreCard }
+
+newGame :: StdGen -> Game
+newGame randomGen = Game { Game.round = newRound randomGen,
+                           scoreCard = newScoreCard }

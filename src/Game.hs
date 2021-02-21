800209cf36b57479f742929e0d635eb11a6213ea
Damien Cassou <damien@cassou.me>
2021-02-21 22:07:44 +0100

    Improve dice selection in Game

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE Safe #-}
 
-module Game (newGame, Game.selectDie, Game.unselectDie, Game.rethrow, Game.writeInBox, Game.isFinished) where
+module Game (newGame, Game.selectDie, Game.selectDice, Game.unselectDie, Game.rethrow, Game.writeInBox, Game.isFinished) where
 
 import System.Random (StdGen)
 
@@ -25,16 +25,22 @@ setScoreCard game scoreCard' = Game { Game.round = Game.round game, scoreCard = 
 -- | Select one of the non-selected dice matching the given
 -- value. Return `Nothing` if the value is not matching any
 -- non-selected dice.
-selectDie :: Game -> Int -> Maybe Game
-selectDie game value = case Round.selectDie (Game.round game) value of
+selectDie :: Int -> Game -> Maybe Game
+selectDie value game = case Round.selectDie (Game.round game) value of
   Just round' -> Just $ setRound game round'
   Nothing -> Nothing
+
+-- | Select non-selected dice matching the given
+-- values. Return `Nothing` if one value is not matching any
+-- non-selected dice.
+selectDice :: [Int] -> Game -> Maybe Game
+selectDice values' game = foldr (maybe Nothing . Game.selectDie) (Just game) values'
 
 -- | Unselect one of the selected dice matching the given
 -- value. Return `Nothing` if the value is not matching any
 -- selected dice.
-unselectDie :: Game -> Int -> Maybe Game
-unselectDie game value = case Round.unselectDie (Game.round game) value of
+unselectDie :: Int -> Game -> Maybe Game
+unselectDie value game = case Round.unselectDie (Game.round game) value of
   Just round' -> Just $ setRound game round'
   Nothing -> Nothing
 

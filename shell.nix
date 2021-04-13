2334fa6dfe022e1a60781a1fcff9ceb947ccd42d
Damien Cassou <damien@cassou.me>
2021-04-13 16:29:42 +0200

    shell.nix: Remove unused stuff

--- shell.nix
+++ shell.nix
@@ -3,20 +3,12 @@
 let
   pkgs = import ./pkgs.nix;
   hsPkgs = pkgs.hsPkgs;
-  ormolu-wrapped =
-    let ormolu = pkgs.haskell-nix.tool hsPkgs.projectArgs.compiler-nix-name "ormolu" "latest";
-    in
-    pkgs.writeShellScriptBin "ormolu" ''
-      ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
-    '';
 in
 hsPkgs.shellFor {
   withHoogle = true;
   tools = {
     cabal = "latest";
-    ghcid = "latest";
     haskell-language-server = "latest";
   };
-  buildInputs = [ ormolu-wrapped ];
   exactDeps = true;
 }

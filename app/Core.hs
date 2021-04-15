2c54e9ef3c80d8ce3ef735e797a758ddd74ba829
Damien Cassou <damien@cassou.me>
2021-04-15 11:23:01 +0200

    Add initial user interface

---
+++ app/Core.hs
@@ -0,0 +1,32 @@
+module Core (GameUI, newGameUI, game, throwDice, toggleDie, tryThrowingDice) where
+
+import qualified Game (Game, toggleDie, rethrow)
+
+newtype GameUI = GameUI { game :: Game.Game}
+
+newGameUI :: Game.Game -> Core.GameUI
+newGameUI game' = GameUI { game = game' }
+
+-- | Select or unselect the die at given index.
+toggleDie :: Int -> Core.GameUI -> Core.GameUI
+toggleDie index gameUI =
+  let game' = Game.toggleDie index $ game gameUI
+  in GameUI {game = game'}
+
+
+-- | Shuffle all unselected dice.
+throwDice :: Core.GameUI -> Core.GameUI
+throwDice gameUI =
+  let
+    game' = game gameUI
+    maybeGame = Game.rethrow game'
+  in
+    maybe gameUI GameUI maybeGame
+
+-- | Throw dice if it is still possible. Otherwise do nothing.
+tryThrowingDice :: Core.GameUI -> Core.GameUI
+tryThrowingDice gameUI =
+  let maybeGame' = Game.rethrow $ game gameUI
+  in case maybeGame' of
+    Nothing -> gameUI
+    Just game' -> GameUI {game = game'}

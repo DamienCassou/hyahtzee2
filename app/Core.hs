cff60de5125ebe41f6698dc82942b428cf02f380
Damien Cassou <damien@cassou.me>
2021-04-17 19:11:57 +0200

    Core: Add functions

--- app/Core.hs
+++ app/Core.hs
@@ -1,6 +1,24 @@
-module Core (GameUI, newGameUI, game, throwDice, toggleDie, tryThrowingDice) where
+module Core
+  ( GameUI
+  , newGameUI
+  , game
+  , throwDice
+  , toggleDie
+  , tryThrowingDice
+  , writeInBox
+  , figureForLetter
+  , letterForFigure
+  ) where
 
-import qualified Hyahtzee2.Game as Game (Game, toggleDie, rethrow)
+import qualified Data.List as List (find, zip)
+
+import qualified Hyahtzee2.Game as Game (Game, toggleDie, rethrow, writeInBox)
+
+import qualified Hyahtzee2.Types as Types
+  ( Figure ( UFigure, LFigure )
+  , UpperFigure(Aces)
+  , LowerFigure
+  )
 
 newtype GameUI = GameUI { game :: Game.Game}
 
@@ -12,7 +30,6 @@ toggleDie :: Int -> Core.GameUI -> Core.GameUI
 toggleDie index gameUI =
   let game' = Game.toggleDie index $ game gameUI
   in GameUI {game = game'}
-
 
 -- | Shuffle all unselected dice.
 throwDice :: Core.GameUI -> Core.GameUI
@@ -30,3 +47,27 @@ tryThrowingDice gameUI =
   in case maybeGame' of
     Nothing -> gameUI
     Just game' -> GameUI {game = game'}
+
+writeInBox :: Types.Figure -> GameUI -> GameUI
+writeInBox figure gameUI =
+  let
+    game' = game gameUI
+    maybeGame = Game.writeInBox figure game'
+  in
+    maybe gameUI GameUI maybeGame
+
+figuresAndLetters :: [(Types.Figure, Char)]
+figuresAndLetters = List.zip allFigures ['a'..'z']
+  where allFigures = upperFigures ++ lowerFigures
+        upperFigures = map Types.UFigure ([minBound .. maxBound] :: [Types.UpperFigure])
+        lowerFigures = map Types.LFigure ([minBound .. maxBound] :: [Types.LowerFigure])
+
+figureForLetter :: Char -> Types.Figure
+figureForLetter letter =
+  let maybePair = List.find (\(_,char) -> char == letter) figuresAndLetters
+  in maybe (Types.UFigure Types.Aces) fst maybePair
+
+letterForFigure :: Types.Figure -> Char
+letterForFigure figure =
+  let maybePair = List.find (\(figure',_) -> figure == figure') figuresAndLetters
+  in maybe 'a' snd maybePair

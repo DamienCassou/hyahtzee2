e1efc548a7ccb95909e272a1cf74fcb0e3b6eab0
Damien Cassou <damien@cassou.me>
2021-04-19 16:03:16 +0200

    Draw: Fix shortcut for die selection

--- app/Draw.hs
+++ app/Draw.hs
@@ -55,7 +55,7 @@ drawHelp :: Core.GameUI -> Brick.Widget Name
 drawHelp gameUI =
   let whenCanThrow =
         if Game.canThrowDice (Core.game gameUI)
-          then "- Press `t` to throw dice again\n" ++ "- Press `1`-`6` to toogle die selection\n"
+          then "- Press `t` to throw dice again\n" ++ "- Press `1`-`5` to toogle die selection\n"
           else ""
       whenCanWrite =
         if not $ Game.isFinished (Core.game gameUI)
